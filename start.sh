12bdc6386baf70f604572710d1625d50621e2cfc
ihackerdoc <ihackerdoc@gmail.com>
2024-09-22 17:34:13 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,25 @@
-if [ -z $UPSTREAM_REPO ]
-then
+#!/bin/bash
+
+# Ensure the script exits if any command fails
+set -e
+
+# Check if UPSTREAM_REPO is set
+if [ -z "$UPSTREAM_REPO" ]; then
   echo "Cloning main Repository"
   git clone https://github.com/lazyindu/test1.git /LazyPrincess
 else
-  echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /LazyPrincess
+  echo "Cloning Custom Repo from $UPSTREAM_REPO"
+  git clone "$UPSTREAM_REPO" /LazyPrincess
 fi
+
+# Navigate to the cloned repository
 cd /LazyPrincess
+
+# Install the required Python packages
 pip3 install -U -r requirements.txt
-echo "Starting Bot...."
+
+# Print a message indicating the start of the bot
+echo "Starting Bot..."
+
+# Run the bot
 python3 bot.py
